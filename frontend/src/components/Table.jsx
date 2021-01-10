7194035ced52ef84279b3c2c94cd817e8375de31
TrevorFlanigan <trevorflanigan@alumni.ubc.ca>
2021-01-09 21:51:36 -0800

    chat frontend logic

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -40,7 +40,28 @@ const useStyles = makeStyles({
     display: "flex",
     flexDirection: "column",
     flex: "1 0 300px",
+    width: "100%",
+    alignItems: "center",
   },
+  chat: {
+    minHeight: "200px",
+    height: "50%",
+    maxHeight: "700px",
+    display: "flex",
+    flexDirection: "column",
+    width: "66%",
+    minWidth: "200px",
+  },
+  upper: {
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+  },
+  lower: {
+    display: "flex",
+    flexBasis: "50px",
+  },
+  textField: {},
 });
 const Table = (props) => {
   let params = useParams();
@@ -50,12 +71,24 @@ const Table = (props) => {
 
   const [name, setName] = useState("");
   const [age, setAge] = useState();
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    socket.on("postMessage", (data) => {
+      setMessages([...messages, data]);
+    });
+  }, []);
 
   const history = useHistory();
 
   const leaveTable = () => {
     socket.emit("leaveRoom", { room: { id }, name: name });
     history.push("/bar");
+  };
+
+  const sendMessage = () => {
+    if (message === "") return;
+    socket.emit("chatMessage", message);
   };
 
   return (
@@ -72,6 +105,18 @@ const Table = (props) => {
           <div className={classes.headerText}>Room {id}</div>
           <div className={classes.body}>
             <p>Welcome to Room {id}!</p>
+            <Paper className={classes.chat}>
+              chat goes here
+              <div className={classes.upper}>upper</div>
+              <div className={classes.lower}>
+                <TextField
+                  label="Chat"
+                  className={classes.textField}
+                  onSubmit={sendMessage}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+              </div>
+            </Paper>
             <Button onClick={() => leaveTable()}>Leave Room</Button>
           </div>
         </Container>
