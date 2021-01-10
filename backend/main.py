213e6b8f81a112b99c8bc9d5a889d92a8d6ed0fd
alexi <a.garbuz@alumni.ubc.ca>
2021-01-09 18:30:24 -0800

    pushing database initial join

--- backend/main.py
+++ backend/main.py
@@ -36,6 +36,13 @@ def test_connect():
     print("Connected")
     emit('after connect',  {'data':'Lets dance'})
 
+#This adds the users data to our database
+@socketio.on('join')
+def join_bar(data):
+    user = Users(name=data['name'],
+                age=data['age'])
+    user.save()
+
 @socketio.on('Wants Random Partner')
 def begin__random_room(sid):
     join_room(sid, 'random1')
